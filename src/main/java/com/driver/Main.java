0a6ff7e3b85bbba272b418d44a5eb35893b8157d
Afzal <af1626112@gmail.com>
2024-01-23 06:19:03 +0530

    attempt 1

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,20 @@
 package com.driver;
 
+// Task 3: Create the main function in Main.java
 public class Main {
-  
+    public static void main(String[] args) {
+        // Task 3: Create an object of class RWOnly in this file
+        RWOnly obj = new RWOnly();
+
+        // Task 4: Try setting a value to name directly and also try printing it
+        // Error: You might see some error, please note it down in comments
+        // obj.name = "Direct Access"; // This line will result in a compilation error
+
+        // Task 5 and Task 6: Try using getter and setter functions
+        // Set a value to name using setter function
+        obj.setName("Set using Setter");
+
+        // Access the value using getter function and print it
+        System.out.println( obj.getName());
+    }
 }
--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -1,5 +1,17 @@
 package com.driver;
 
-public class RWOnly {
-    
-}
+    public class RWOnly {
+        // Private member variable
+        private String name;
+
+        // Task 5: Implement getter function for the private member
+        public String getName() {
+            return name;
+        }
+
+        // Task 6: Implement setter function for the private member
+        public void setName(String name) {
+            this.name = name;
+        }
+    }
+
